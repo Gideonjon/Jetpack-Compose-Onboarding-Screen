32cc9bca1ba376a53287aa1c3c0a2b0cf8b59343
gideonjon <gideonjones63@gmail.com>
2024-07-22 18:54:09 +0100

    added-newly

--- app/src/main/java/com/dash/foodcart/MainActivity.kt
+++ app/src/main/java/com/dash/foodcart/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,19 +18,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContent {
-
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-
+            FoodCartTheme {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
                 ) {
-
+                    Greeting(name = "Gideon Jones!")
+                }
             }
-
-            Greeting("Android")
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -44,10 +42,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Surface {
-        FoodCartTheme {
-            Greeting("Android")
-
-        }
+    FoodCartTheme {
+        Greeting("Android")
     }
+
 }
